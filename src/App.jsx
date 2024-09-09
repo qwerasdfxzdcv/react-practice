2ad842e5b819d002b8ce999cae6ad3b67d12ee27
qwerasdfxzdcv <104199005+qwerasdfxzdcv@users.noreply.github.com>
2024-09-10 00:08:22 +0900

    close #9

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,39 @@
 import './App.css';
 import { useState } from 'react';
-import CalcHandler from './components/CalcHander';
 //component .jsx
 function App() {
   
   const [num, setNum] = useState("0")
   const [result, setResult] = useState("0")
   const [logs,setLogs] = useState([0])
+  const [logs_middle,setLogs_middle] = useState([0])
   const numHandler = (e) =>{ setNum(parseFloat(e.target.value))}
   const CalcHandler = (e) =>{
-    if(e.target.innerHTML === '+') setResult(parseFloat(result + num))
-    if(e.target.innerHTML === '-') setResult(parseFloat(result - num))
-    if(e.target.innerHTML === 'x') setResult(parseFloat(result * num))
-    if(e.target.innerHTML === '/') setResult(parseFloat(result / num))
+    if(e.target.innerHTML === '+'){
+      setResult(parseFloat(result + num))
+      setLogs([...logs,parseFloat(result + num)])
+      setLogs_middle([...logs_middle," + " + num])
+    }
+    if(e.target.innerHTML === '-'){
+      setResult(parseFloat(result - num))
+      setLogs([...logs,parseFloat(result - num)])
+      setLogs_middle([...logs_middle," - " + num])
+    }
+    if(e.target.innerHTML === 'x'){
+      setResult(parseFloat(result * num))
+      setLogs([...logs,parseFloat(result * num)])
+      setLogs_middle([...logs_middle," * " + num])
+    }
+    if(e.target.innerHTML === '/'){
+      setResult(parseFloat(result / num))
+      setLogs([...logs,parseFloat(result / num)])
+      setLogs_middle([...logs_middle," / " + num])
+    }
   }
 
   return (
-    <div>
       <div className="App">
+        <div>
         <input type='number' onChange={numHandler}></input>
       </div>
       <div>
@@ -26,8 +42,13 @@ function App() {
         <button onClick={CalcHandler} >x</button>
         <button onClick={CalcHandler} >/</button>
       </div>
-      {result}
-    </div>
+      <div>
+        {logs.map((result,i)=>i===0 ? <p key = {i}>초기값 = {result}</p>:
+        <p key={i}>{logs[i-1]}  {logs_middle[i]} = {logs[i]}</p>)
+        }
+        </div>
+        
+      </div>
   );
 }
 
